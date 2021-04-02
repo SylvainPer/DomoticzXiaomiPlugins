d3cd19461fa712f2005b8b02ad6144e8368321d8
Sylvain PERON <48243214+SylvainPer@users.noreply.github.com>
2021-04-02 21:31:57 +0200

    AirPurifier_AQI was not updated correctly

--- MiioAirPurifierMiot/plugin.py
+++ MiioAirPurifierMiot/plugin.py
@@ -583,11 +583,11 @@ class AirPurifierMiotPlugin:
         {
             "_Name": "AirPurifier_AQI", 
             "_Unit": __UNIT_AQI, 
-            "_TypeName": "Air Quality",
-            "_Options": None,
-            # {
-                # "Custom": "1;μg/m³"
-            # },
+            "_TypeName": "Custom",
+            "_Options":
+            {
+                "Custom": "1;μg/m³"
+            },
             "bindingStatusField": "aqi",
             "mapStatus": MapStatus,
             "map_status_value": 1, 
